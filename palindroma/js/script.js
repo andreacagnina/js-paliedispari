b24fe0a181888be04e5a8c534591acb4804b3803
andreacagnina <amirkal@libero.it>
2024-07-04 17:35:51 +0200

    [FEAT] da metabolizzare l'utilizzo del valore del parametro della funziona

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,19 +1,46 @@
-let word = prompt('Inserisci una parola o una frase e ti dirò se è palindroma!');
+// let word = prompt('Inserisci una parola o una frase e ti dirò se è palindroma!');
 
-let wordsNoSpace = word.replace(/ /g, "");
+// let wordsNoSpace = word.replace(/ /g, "");
 
-console.log(wordsNoSpace)
+// console.log(wordsNoSpace)
 
 
-function SplitReverseJoin(palindroma) {
-    let reverse = wordsNoSpace.split('').reverse().join('');
+// function SplitReverseJoin(palindroma) {
+//     let reverse = wordsNoSpace.split('').reverse().join('');
 
-    if (reverse === wordsNoSpace) {
-        alert('Palindroma')
+//     if (reverse === wordsNoSpace) {
+//         alert('Palindroma')
+//     }
+//     else {
+//         alert('Non Palindroma')
+//     }
+//     console.log(reverse)
+// }
+// SplitReverseJoin(word);
+
+// let word = prompt('Inserisci una parola!');
+
+function pal(pal) {
+
+    let stringa = ('');
+    let len = pal.length;
+
+    for (let i = len - 1; i >= 0; i--) {
+        console.log(pal[i])
+        stringa += pal[i]
+
+    }
+    if (stringa === pal) {
+        console.log('palindroma')
     }
     else {
-        alert('Non Palindroma')
+        console.log('non palindroma')
     }
-    console.log(reverse)
 }
-SplitReverseJoin(word);
+
+pal(word);
+
+
+
+
+
