24037d8c8c41b950dc9263840c7fb3cc6a00635e
andreacagnina <amirkal@libero.it>
2024-07-04 20:51:08 +0200

    [FEAT] ADD LOWERCASE CONDITION

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -1,38 +1,46 @@
-let scommessa = prompt('Scegli se il numero sarà pari o dispari');
-console.log(scommessa);
-let scelta = scommessa.toLowerCase;
 
-let lancio = parseInt(prompt('Scrivi un numero da 1 a 5'));
-console.log(lancio);
 
-let RandomPc = lancioPC();
-console.log(RandomPc)
+for (let i = 0; i < 1; i++) {
+    let scommessa = prompt('Scegli se il numero sarà pari o dispari').toLowerCase();
+    while (scommessa != 'pari' && scommessa != 'dispari') {
+        scommessa = prompt('DEVI scegliere se il numero sarà pari o dispari').toLowerCase();
+    }
+    let lancio = parseInt(prompt('Scrivi un numero da 1 a 5'));
+    console.log(lancio);
 
-function lancioPC() {
-    return Math.floor(Math.random() * 5 + 1);
-}
+    while (isNaN(lancio)) {
+        lancio = parseInt(prompt('Il valore inserito non è valido. Inserisci un numero da 1 a 5'));
+    }
 
-let sommaLanci = RandomPc + lancio;
-console.log(sommaLanci)
 
-EorO();
+    let RandomPc = lancioPC();
+    console.log(RandomPc)
 
-function EorO() {
-    if (sommaLanci % 2 === 0) {
-        console.log('Pari')
+    function lancioPC() {
+        return Math.floor(Math.random() * 5 + 1);
+    }
+
+    let sommaLanci = RandomPc + lancio;
+    console.log(sommaLanci)
+
+    EorO();
+
+    function EorO() {
+        if (sommaLanci % 2 === 0) {
+            console.log('Pari')
+        }
+        else {
+            console.log('Dispari')
+        }
+    }
+
+    if ((scommessa === 'pari' && sommaLanci % 2 == 0) || (scommessa === 'dispari' && sommaLanci % 2 !== 0)) {
+        console.log('Hai vinto una macchina')
     }
     else {
-        console.log('Dispari')
+        console.log('Hai perso casa tua')
     }
 }
-
-if ((scommessa === 'pari' && sommaLanci % 2 == 0) || (scommessa === 'dispari' && sommaLanci % 2 !== 0)) {
-    console.log('Hai vinto una macchina')
-}
-else {
-    console.log('Hai perso casa tua')
-}
-
 
 
 
