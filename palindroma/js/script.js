69143cb96e3a69d3ea61ac06fbd38ecdcdfba34b
andreacagnina <amirkal@libero.it>
2024-07-04 15:57:37 +0200

    [feat] tryed with return

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,14 +1,15 @@
 let word = prompt('Inserisci una parola');
-let reverse
+
 
 function pal(word) {
     reverse = word.split('').reverse().join('');
-    if (word == reverse) {
+    if (word === reverse) {
         alert('Palindroma')
     }
     else {
         alert('Non Palindroma')
     }
+
 }
 
 pal(word);
