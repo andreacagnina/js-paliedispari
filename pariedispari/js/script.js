357cadd8504004af1245e38aef6795662de99b81
andreacagnina <amirkal@libero.it>
2024-07-04 18:46:02 +0200

    [FEAT] created fun somma

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -1,5 +1,6 @@
 let scelta = prompt('Scegli se il numero sarà pari o dispari');
 let lancio = parseInt(prompt('Scrivi un numero da 1 a 5'));
+console.log(lancio)
 
 function lancioPC() {
     return Math.floor(Math.random() * 5 + 1);
@@ -7,3 +8,10 @@ function lancioPC() {
 
 let RandomPc = lancioPC();
 console.log(RandomPc)
+
+let sommaLanci = somma();
+
+function somma() {
+    return somma = RandomPc + lancio
+}
+console.log(sommaLanci)
